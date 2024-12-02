fc36202b3609a95ae46b580db43660fb9ab426f6
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-02 12:18:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed quo eum nesciunt repellendus libero fugit ad facere,
+        exercitationem ratione facilis,
+        eligendi cupiditate debitis velit. Laborum non consequatur quaerat beatae? Earum!
+      </p>
+    </>
+  )
+}
+
+export default App;

ba9e7261b61eea2d7f9b67170dff3141ddd603b6
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-03 09:10:35 -0300

    style effects on scroll, grid and grid items

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import { PokemonData } from "@/types/types";
 import "@styles/ListItem.css";
 
 interface Props {
-    pokemon: PokemonData
+    pokemon: PokemonData;
 }
 
 export const ListItem = ({ pokemon }: Props) => {
-    return(
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setIsLoading(false);
+    };
+
+    return (
         <div className="item">
-            <img src={`${pokemon.imageUrl}`} alt={`${pokemon.name} image`} />
+            <div>
+                {isLoading && (
+                    <div className="placeholder">
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                            <path stroke="#0A0A30" stroke-linecap="round" stroke-width="1.5"
+                                d="M12 6.864v1.333m0 7.606v1.333M17.136 12h-1.333m-7.606 0H6.864m8.768 3.632l-.943-.943M9.311 9.311l-.943-.943m0 7.264l.943-.943m5.378-5.378l.943-.943"
+                                style={{ animation: "loader4 1.5s linear infinite both", transformOrigin: "center center"}} />
+                        </svg>
+                    </div>
+                )}
+                <img
+                    src={`${pokemon.imageUrl}`}
+                    alt={`${pokemon.name} image`}
+                    onLoad={handleImageLoad}
+                    className={isLoading ? "hidden" : ""}
+                />
+            </div>
             <p>{pokemon.name}</p>
         </div>
     );
-} 
+};

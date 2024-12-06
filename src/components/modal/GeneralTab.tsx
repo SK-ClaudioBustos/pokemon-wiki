f9d9d7cf367e67e8eb0b7eeab517c27357efa2c2
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-06 10:55:08 -0300

    improve responsive

--- src/components/modal/GeneralTab.tsx
+++ src/components/modal/GeneralTab.tsx
@@ -52,12 +52,14 @@ export const GeneralTab = () => {
                 <div className="details-img">
                     <button style={{ "--shiny-color": showShiny ? "#fce024" : "#c1c1c1" } as React.CSSProperties} onClick={() => setShowShniy(!showShiny)} className="shiny-icon">
                         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#f1f1f1">
-                            <path style={{ stroke: "#fff", strokeWidth: "1" }} d="M11.081 5.141c.347-.807 1.491-.807 1.838 0l1.274 2.97a1 1 0 00.828.601l3.218.295c.875.08 1.229 1.168.568 1.748l-2.43 2.13a1 1 0 00-.316.972l.714 3.152c.194.857-.732 1.53-1.487 1.08l-2.776-1.653a1 1 0 00-1.024 0l-2.776 1.653c-.755.45-1.681-.223-1.487-1.08l.715-3.152a1 1 0 00-.317-.973l-2.43-2.13c-.66-.579-.307-1.667.568-1.747l3.218-.295a1 1 0 00.828-.601l1.274-2.97z" />
+                            <path
+                                style={{ stroke: "#fff", strokeWidth: "1" }}
+                                d="M11.081 5.141c.347-.807 1.491-.807 1.838 0l1.274 2.97a1 1 0 00.828.601l3.218.295c.875.08 1.229 1.168.568 1.748l-2.43 2.13a1 1 0 00-.316.972l.714 3.152c.194.857-.732 1.53-1.487 1.08l-2.776-1.653a1 1 0 00-1.024 0l-2.776 1.653c-.755.45-1.681-.223-1.487-1.08l.715-3.152a1 1 0 00-.317-.973l-2.43-2.13c-.66-.579-.307-1.667.568-1.747l3.218-.295a1 1 0 00.828-.601l1.274-2.97z" />
                         </svg>
                     </button>
                     <img src={showShiny ? pokemonData.img.shiny : pokemonData.img.normal} alt={`image of ${pokemonData.name}`} />
                 </div>
-                <div>
+                <div className="details-pokemon">
                     <div className="details-stats">
                         <ul>
                             {
